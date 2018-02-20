5485fe464552582e0b728f49a7105f86958ab64c
Eugene Toropov <eugene.toropov@gmail.com>
2018-02-20 10:46:36 +0300

    Export JSON utils

--- json.go
+++ json.go
@@ -8,8 +8,8 @@ import (
 	"io/ioutil"
 )
 
-// encodeString encodes a string into Base64
-func encodeString(i map[string]interface{}) (string, error) {
+// EncodeString encodes a string into Base64
+func EncodeString(i map[string]interface{}) (string, error) {
 	b, err := json.Marshal(i)
 	if err != nil {
 		return "", err
@@ -19,23 +19,23 @@ func encodeString(i map[string]interface{}) (string, error) {
 	return string(dst), nil
 }
 
-// decodeString decodes a Base64 encoded string
-func decodeString(s string) (map[string]interface{}, error) {
+// DecodeString decodes a Base64 encoded string
+func DecodeString(s string) (map[string]interface{}, error) {
 	dst := make([]byte, base64.StdEncoding.DecodedLen(len(s)))
 	n, err := base64.StdEncoding.Decode(dst, []byte(s))
 	if err != nil {
 		return nil, err
 	}
 	var v map[string]interface{}
-	if err := unmarshal(bytes.NewReader(dst[:n]), &v); err != nil {
+	if err := Unmarshal(bytes.NewReader(dst[:n]), &v); err != nil {
 		return nil, err
 	}
 	return v, nil
 }
 
-// unmarshal unmarshalles a Reader Object and returns as a mapped string interface the JSON document
+// Unmarshal unmarshalles a Reader Object and returns as a mapped string interface the JSON document
 // See https://ahmetalpbalkan.com/blog/golang-json-decoder-pitfalls/ for why to change from decode.
-func unmarshal(r io.Reader, obj interface{}) error {
+func Unmarshal(r io.Reader, obj interface{}) error {
 	data, err := ioutil.ReadAll(r)
 	if err != nil {
 		return err
