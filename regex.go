74076a640606836bff8bb39028f04b6083b81496
Glenn Walker <glennwalker3976@gmail.com>
2019-03-13 15:57:17 +0000

    Update regex.go
    Update Regular Expression utils.

--- regex.go
+++ regex.go
@@ -40,7 +40,27 @@ func RegExSearchAny(search string) string {
 	return (ptn)
 }
 
-// StartsWithVowel checks if string starts with vowel
+// RegExSearchAll generates a fuzzy RegEx search pattern for given search text where all match
+func RegExSearchAll(search string) string {
+	// Split search string on spaces
+	ptnS := "(?=.*"
+	ptnE := ")"
+	ptn := ptnS + search + ptnE
+	// Return the compiled RegEx pattern
+	return (ptn)
+}
+
+// RegExSearchAllStartsWith generates a fuzzy RegEx search pattern for given search text where start of word matches
+func RegExSearchAllStartsWith(search string) string {
+	// Split search string on spaces
+	ptnS := "(^"
+	ptnE := ")"
+	ptn := ptnS + search + ptnE
+	// Return the compiled RegEx pattern
+	return (ptn)
+}
+
+// StartsWithVowel checks if a string starts with a voewel
 func StartsWithVowel(s string) bool {
 	if len(s) > 0 {
 		s_token := Tokenize(s)
@@ -63,7 +83,7 @@ func StartsWithVowel(s string) bool {
 	return false
 }
 
-// AdjustForVowel adjusts string for vowel
+// AdjustForVowel returns a word to append if a given word if the word starts with a vowel
 func AdjustForVowel(word string, appendword string) string {
 	if len(word) > 0 {
 		if StartsWithVowel(word) {
@@ -73,7 +93,7 @@ func AdjustForVowel(word string, appendword string) string {
 	return ""
 }
 
-// RemoveNonAlphaNumeric removes non alpha numeric characters from a string
+// RemoveNonAlphaNumeric is a regex to remove any non-alphanumeric characters from a string
 func RemoveNonAlphaNumeric(s string) string {
 	// Make a Regex to say we only want alphanumeric characters
 	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
