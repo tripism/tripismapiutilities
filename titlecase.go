eae39abb15cca26cfb1fbe29f62ad8b639d92816
Glenn Walker <glennwalker3976@gmail.com>
2019-05-08 09:45:20 +0100

    Update titlecase.go
    Tweak input variable name and comments.

--- titlecase.go
+++ titlecase.go
@@ -3,9 +3,10 @@ package tripismapiutilities
 import "strings"
 
 // TitleCase returns title cased string :) all letters that begin words mapped to their title case.
-// Certain small words skipped though.
-func TitleCase(input string) string {
-	words := strings.Fields(input)
+// Certain small words are skipped to ensure grammatical correctness.
+// e.g. WELCOME TO THE DOLLHOUSE => Welcome to the Dollhouse
+func TitleCase(s string) string {
+	words := strings.Fields(s)
 	smallwords := " a an on the to of in and "
 
 	for index, word := range words {
