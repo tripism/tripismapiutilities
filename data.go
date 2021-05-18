1660d5f5c1b7f9d2180591af974d0ec95a0501ad
Glenn Walker <glennwalker3976@gmail.com>
2021-05-18 10:58:37 +0100

    Create data.go
    Added method to clean website query string parameters - used when importing hotel data / updating card details.website values.

---
+++ data.go
@@ -0,0 +1,27 @@
+package tripismapiutilities
+
+import "net/url"
+
+// CleanWebsiteQueryString removes unused / tracking query string parameters from website URLs
+//	for example, it deletes all "utm_source", "utm_campaign" and "utm_medium" Google Analytcs parameters
+func CleanWebsiteQueryString(uri string) string {
+	u, err := url.Parse(uri)
+	if err != nil {
+		return uri
+	}
+
+	q := u.Query()
+	q.Del("scid")
+	q.Del("cid")
+	q.Del("y_source")
+	q.Del("utm_source")
+	q.Del("utm_campaign")
+	q.Del("utm_medium")
+	q.Del("utm_term")
+	q.Del("WT.mc_id")
+	q.Del("SEO_id")
+	q.Del("s_cid")
+
+	u.RawQuery = q.Encode()
+	return u.String()
+}
